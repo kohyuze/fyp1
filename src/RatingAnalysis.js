5e1c2d35274e83a9d6fe669de4c6a2440f73541c
kohyuze <kohyuze@gmail.com>
2021-07-24 20:29:53 +0800

    fixed the start page links

--- src/RatingAnalysis.js
+++ src/RatingAnalysis.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const RatingAnalysis = () => {
     return ( 
         <p>RatingAnalysis page</p>
--- src/Start.js
+++ src/Start.js
@@ -2,21 +2,21 @@ import { Link } from 'react-router-dom';
 
 const Start = () => {
     return (
-        <div className="start">
+        <div className="container">
             <h1 className="startTitle">Thermal and Hydraulic Analysis</h1>
             <div className="nav">
                 <div className="navButtons" >
-                    <Link to={"/RatingAnalysis"}>
+                    <Link to={"/RatingAnalysis"} className="navLink">
                         <h2 className="buttonText">Rating Analysis</h2>
                     </Link>
                 </div>
                 <div className="navButtons" >
-                    <Link to={"/SizingAnalysis"}>
+                    <Link to={"/SizingAnalysis"} className="navLink">
                         <h2 className="buttonText">Sizing Analysis</h2>
                     </Link>
                 </div>
                 <div className="navButtons" >
-                    <Link to={"/Data"}>
+                    <Link to={"/Data"} className="navLink">
                         <h2 className="buttonText">Data</h2>
                     </Link>
                 </div>
